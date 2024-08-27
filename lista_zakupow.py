c9c2eac9d55a1b42455bc60ffe1109111cc7a300
Maja Skóbel <maja.skobel01@gmail.com>
2024-08-27 21:15:56 +0100

    Add loop for shopping list:

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,9 @@ shopping_list = {
     "piekarnia": ["chleb","bułki","pączki"],
     "warzywniak": ["marchew","seler","rukola"]
 }
+
+print("Lista zakupów:")
+
+for shop,product_list in shopping_list.items():
+    product_list_updated = [product.title() for product in product_list]
+    print(f"Idę do {shop.title()} i kupuję tam {product_list_updated}.")

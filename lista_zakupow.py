4911905646805e022a56a3f1eb26e1251787ba16
Maja Skóbel <maja.skobel01@gmail.com>
2024-08-27 21:17:11 +0100

    Add number of products bought

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -4,7 +4,11 @@ shopping_list = {
 }
 
 print("Lista zakupów:")
+sum_of_products=0
 
 for shop,product_list in shopping_list.items():
     product_list_updated = [product.title() for product in product_list]
+    sum_of_products=sum_of_products+len(product_list)
     print(f"Idę do {shop.title()} i kupuję tam {product_list_updated}.")
+
+print(f"W sumie kupuję {sum_of_products} produktów.")

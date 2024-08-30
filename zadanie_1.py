ed5b37335b1d83835082ffb0d6909ddc262b7d22
Maja Skóbel <maja.skobel01@gmail.com>
2024-08-30 23:43:39 +0100

    Add documentation

--- zadanie_1.py
+++ zadanie_1.py
@@ -1,4 +1,8 @@
 def palindrome(word):
+    """
+    Returns true if a word is a palindrome (word that reads the same forwards as backwards), otherwise false.
+    Returns none if the input was not a word.
+    """
     if not isinstance(word, str):
         return None
     word_letters = [letter for letter in word]
